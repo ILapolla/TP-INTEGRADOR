a38db3ce568616f6d6dfad2edf2d4d428fca3cff
Aylensol96 <115493072+Aylensol96@users.noreply.github.com>
2022-11-10 21:47:24 -0300

    Create comprar-tickets.js

---
+++ comprar-tickets.js
@@ -0,0 +1,9 @@
+const TICKET  = 200
+const nombre = document.getElementById("nombre");
+const apellido = document.getElementById("apellido");
+const email = document.querySelector("#email");
+const tickets = document.querySelector(cantidaddetickets");
+const categoria = document.querySelector("categorias");
+const pago = document.querySelector("#pago");
+
+
