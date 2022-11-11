d98f3ed503a45b6504d0dc8f7c7fb8c642d039a6
Aylensol96 <115493072+Aylensol96@users.noreply.github.com>
2022-11-11 00:14:10 -0300

    Update tickets.js

--- tickets.js
+++ tickets.js
@@ -7,3 +7,7 @@ const categoria = document.querySelector("categorias");
 const pago = document.querySelector("#pago");
 
 
+const expresiones = {
+	nombre: /^[a-zA-Z0-9\_\-]{4,16}$/,
+	apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
